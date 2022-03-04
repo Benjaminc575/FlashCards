bc639f9abd47cc041758cba11c90c8d01bf3b051
Benjamin Cardenas <benjaminc5751@gmail.com>
2022-03-03 23:59:54 -0600

    Added Toggle and styling to flashcard.

--- FlashCards/Readme.md
+++ FlashCards/Readme.md
@@ -20,6 +20,6 @@ Simple FlashCard App that provides a question and an answer when tapped.
 - [x] Build in logic to show the answer side when the card is tapped
 - [x] Push code to GitHub
 ## Optional
-- [ ] Toggle the flashcard between the question side and the answer side
-- [ ] Style the question and answer side of the card to better distinguish between the two sides
+- [x] Toggle the flashcard between the question side and the answer side
+- [x] Style the question and answer side of the card to better distinguish between the two sides
 - [ ] Add selectable multiple choice answers beneath the card
--- FlashCards/ViewController.swift
+++ FlashCards/ViewController.swift
@@ -12,10 +12,43 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var card: UIView!
+    
+    @IBOutlet weak var btnOptionOne: UIButton!
+    
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    
+    @IBOutlet weak var btnOptionThree: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        card.layer.cornerRadius = 20.0 // creates round corners for card
+        card.layer.shadowRadius = 15.0 // shadow radius for card
+        card.layer.shadowOpacity = 0.3 //shadows opacity for card
+        
+        frontLabel.layer.cornerRadius = 15.0 // creates rounded card
+        frontLabel.clipsToBounds = true // clip front label inside
+        backLabel.layer.cornerRadius = 20.0 // creates rounded card
+        backLabel.clipsToBounds = true // clip front label inside
+        
+        //creates the outline and color for button 1
+        btnOptionOne.layer.cornerRadius = 15.0
+        btnOptionOne.layer.borderWidth = 3.0
+        btnOptionOne.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        //creates the outline and color for button 2
+        btnOptionTwo.layer.cornerRadius = 15.0
+        btnOptionTwo.layer.borderWidth = 3.0
+        btnOptionTwo.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        //creates the outline and color for button 3
+        btnOptionThree.layer.cornerRadius = 15.0
+        btnOptionThree.layer.borderWidth = 3.0
+        btnOptionThree.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        
         
     }
     
@@ -26,8 +59,26 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func didTapOptionOne(_ sender: Any) {
+        btnOptionOne.isHidden = true
+    }
+    
+    @IBAction func didTapOptionTwo(_ sender: Any) {
+        frontLabel.isHidden = !frontLabel.isHidden
+    }
+    
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        btnOptionThree.isHidden = true
+        
+    }
+    
+    
+
+}
+    
+    
+    
         
     
     
-}
 
